6c2da1014a8c8eb2558a622a3145c6f9a81bed87
Eternity <Eternity@hellodomain.local>
2016-04-18 00:07:28 +0200

    Changed a few things in the script.

--- ScriptADBetter.ps1
+++ ScriptADBetter.ps1
@@ -1,7 +1,6 @@
 ﻿# Requires User Input
 $CSV = Read-Host 'Please specify the path of your .csv'
 $COMP = Read-Host 'Please specify your computer name'
-$OU = Read-Host 'Please specify an OU'
 $DC1 = Read-Host 'Please specify a DC' 
 $DC2 = Read-Host 'Please specify a DC extension'
 
@@ -11,7 +10,11 @@ Import-Csv $CSV | ForEach-Object {Write-Host $_."Name"} {
 # Import Variables
 $LogonUsername = $_."LogonUsername" 
 $Name = $_."Name"
-$Displayname = $_."Displayname"
+$GivenName = $_. "GivenName"
+$OtherName = $_. "OtherName"
+$Surname = $_. "Surname"
+$Displayname = $Name
+$OU = $_. "OU"
 
 # Create Folder
 New-Item -Path C:\Hey\$LogonUsername -Force -Type Directory  
@@ -26,13 +29,16 @@ New-ADUser `
 -AccountPassword (ConvertTo-SecureString "Welkom01" -AsPlainText -Force) `
 -ChangePasswordAtLogon $true `
 -Enabled $true `
+-GivenName $GivenName `
+-OtherName $OtherName `
+-Surname $Surname `
 -HomeDrive "H:" `
 -HomeDirectory "\\2016test1\$Name\UserData" `
 -ProfilePath: "\\2016test1\$Name\"
-Add-ADGroupMember "Group1" "$LogonUsername"
+# Add-ADGroupMember "Group1" "$LogonUsername"
 
 # Share folder and set rights
-New-SmbShare  -Name $Name -Path C:\Hey\$LogonUsername -ChangeAccess $LogonUsername
+New-SmbShare  -Name "$Name" -Path C:\Hey\$LogonUsername -ChangeAccess $LogonUsername
 
 # Set NTFS
 Icacls C:\Hey\$LogonUsername /inheritance:r /grant:r ${LogonUsername}:"(OI)(CI)M"
