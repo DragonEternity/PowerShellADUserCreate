23452f7fc86165b6d9a8750406fa16430484d72a
Eternity <Eternity@REMCO.LOCAL>
2016-04-20 11:37:54 +0200

    Redid the entire thing for a school project.

--- ScriptADBetter.ps1
+++ ScriptADBetter.ps1
@@ -4,58 +4,60 @@ $COMP = Read-Host 'Please specify your computer name'
 $OU = Read-Host 'Please specify an OU'
 $DC1 = Read-Host 'Please specify a DC' 
 $DC2 = Read-Host 'Please specify a DC extension'
+$Map = Read-Host 'Please specify a destination for the userdata'
 
 # Import CSV
 Import-Csv $CSV | ForEach-Object {Write-Host $_."Name"} {
 
 # Import Variables
- 
+
+$Name = $_. "EmployeeID"
 $GivenName = $_. "GivenName"
 $OtherName = $_. "OtherName"
 $Surname = $_. "Surname"
-$Name = $_. "$GivenName.$Surname"
-$Displayname = $Name
-$LogonUsername = $_. $Name
+$Displayname = $_. "GivenName"
+$LogonUsername = $_. "EmployeeID"
 $EmailAddress = $_. "EmailAddress"
 $EmployeeID = $_. "EmployeeID"
 $Division = $_. "Division"
 $EnrollmentDate = $_. "Date"
 
 # Create Folder
-New-Item -Path C:\Hey\$LogonUsername -Force -Type Directory  
-New-Item -Path C:\Hey\$LogonUsername\UserData -Force -Type Directory 
+New-Item -Path $Map\$LogonUsername -Force -Type Directory  
+New-Item -Path $Map\$LogonUsername\UserData -Force -Type Directory 
 
 # Create user and set stuff
 New-ADUser `
--Name $Name `
+-Name "$Name" `
 -Path "OU=$OU,DC=$DC1,DC=$DC2" `
--SamAccountName $LogonUsername `
--Displayname $Displayname `
+-SamAccountName "$LogonUsername" `
+-Displayname "$Displayname" `
 -AccountPassword (ConvertTo-SecureString "Welkom01" -AsPlainText -Force) `
 -ChangePasswordAtLogon $true `
 -Enabled $true `
--GivenName $GivenName `
--EmailAddress $EmailAddress `
--EmployeeID = $EmployeeID `
--Divison = $Division `
--Description = $EnrollmentDate
--OtherName $OtherName `
--Surname $Surname `
+-GivenName "$GivenName" `
+-EmailAddress "$EmailAddress" `
+-Division "$Division" `
+-Description "$EnrollmentDate" `
+-EmployeeID "$EmployeeID" `
+-OtherName "$OtherName" `
+-Surname "$Surname" `
 -HomeDrive "H:" `
 -HomeDirectory "\\2016test1\$Name\UserData" `
 -ProfilePath: "\\2016test1\$Name\"
 # Add-ADGroupMember "Group1" "$LogonUsername"
 
 # Share folder and set rights
-New-SmbShare  -Name "$Name" -Path C:\Hey\$LogonUsername -ChangeAccess $LogonUsername
+New-SmbShare  -Name "$Name" -Path $Map\$LogonUsername -ChangeAccess $LogonUsername
 
 # Set NTFS
-Icacls C:\Hey\$LogonUsername /inheritance:r /grant:r ${LogonUsername}:"(OI)(CI)M"
+Icacls $Map\$LogonUsername /inheritance:r /grant:r ${LogonUsername}:"(OI)(CI)M"
 
 # Check if the user is there and write to log.
 if( Get-ADUser -f "SamAccountName -like '$LogonUsername'")
 {write-host "Created $Name"
-  Out-File -FilePath C:\Hey\Log.txt -inputobject "Created $Name" -append}
+  Out-File -FilePath $Map\Log.txt -inputobject "Created $Name" -append}
   else {write-host "Failed to create $Name"}
 
 } | Out-Null
+#
